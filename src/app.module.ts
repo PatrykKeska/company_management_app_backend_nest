b38b5efb7a1f42a755b2867801bb99bbe7fa244a
PatrykKeska <PatrykKeska4@gmail.com>
2022-09-09 11:06:31 +0200

    * Auth Module Controller Service created * Passport.js, CookieParser added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 import dbConfig from './config/dbConfig';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(dbConfig()),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
