2699e33d58868e6b4b1ab5d2a83db37491a1716d
PatrykKeska <PatrykKeska4@gmail.com>
2022-09-09 10:08:08 +0200

    * added nullable false to all rows

--- src/entities/places.entity.ts
+++ src/entities/places.entity.ts
@@ -8,30 +8,35 @@ export class Places extends BaseEntity {
   @Column({
     length: 20,
     type: 'varchar',
+    nullable: false,
   })
   name: string;
 
   @Column({
     length: 35,
     type: 'varchar',
+    nullable: false,
   })
   city: string;
 
   @Column({
     length: 36,
     type: 'varchar',
+    nullable: false,
   })
   street: string;
 
   @Column({
     length: 20,
     type: 'varchar',
+    nullable: false,
   })
   buildNumber: string;
 
   @Column({
     length: 200,
     type: 'varchar',
+    nullable: false,
   })
   img: string;
 }
