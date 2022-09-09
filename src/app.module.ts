72db3905b52f96613a2b6d56e245368dfcb2fd61
PatrykKeska <PatrykKeska4@gmail.com>
2022-09-09 12:35:46 +0200

    * userModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 import dbConfig from './config/dbConfig';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(dbConfig()),
     AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
