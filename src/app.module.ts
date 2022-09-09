f9daa57aba3943475e9a9bb0659cbb38b988a408
Patryk Keska <45535706+PatrykKeska@users.noreply.github.com>
2022-09-09 09:19:48 +0200

    Revert "Feature/folder structure"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import dbConfig from './config/dbConfig';
+
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot(dbConfig()),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
