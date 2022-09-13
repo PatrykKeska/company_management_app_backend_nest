61cb3817672666ea5bf549b57b06fe534c100c56
PatrykKeska <PatrykKeska4@gmail.com>
2022-09-13 19:41:17 +0200

    * Product In places controller added a new api endpoint /assign which allow assigning a product to location(place/office)

--- src/product-in-places/product-in-places.controller.ts
+++ src/product-in-places/product-in-places.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { ProductInPlacesService } from './product-in-places.service';
+import { AddProductToPlaceDto } from './dto/add-product-to-place.dto';
+import { ProductAssignToPlaceResponse } from './interfaces/product-assign-to-place-response';
 
 @Controller('product-in-places')
-export class ProductInPlacesController {}
+export class ProductInPlacesController {
+  constructor(
+    @Inject(ProductInPlacesService)
+    private productInPlacesService: ProductInPlacesService,
+  ) {}
+
+  @Post('/assign')
+  async assignProductToPlace(
+    @Body() data: AddProductToPlaceDto,
+  ): Promise<ProductAssignToPlaceResponse> {
+    return this.productInPlacesService.addProductToPlace(data);
+  }
+}
