2d1c2dec7aa9ef3df0952e000ec0dffbe8defcff
PatrykKeska <PatrykKeska4@gmail.com>
2022-09-15 20:14:14 +0200

    * ProductService can handle : * file transfer for each product * unLink a file if something went wrong * updating the file including a file ( old file unlink)

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { Products, ProductStatus } from '../entities/products.entity';
 import { DataSource } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -7,10 +7,19 @@ import { ProductUpdatedResponse } from './interfaces/product-updated-response';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { ProductInPlaces } from '../entities/product_in_places.entity';
 import { RemoveProductResponse } from './interfaces/remove-product-response';
+import { FileTransferInterface } from '../file-transfer/interfaces/multer-disk-uploaded-files';
+import { FileTransferService } from '../file-transfer/file-transfer.service';
+import * as fs from 'fs';
+import * as path from 'path';
+import { storageDir } from '../utils/storage';
 
 @Injectable()
 export class ProductsService {
-  constructor(@Inject(DataSource) private dataSource: DataSource) {}
+  constructor(
+    @Inject(DataSource) private dataSource: DataSource,
+    @Inject(forwardRef(() => FileTransferService))
+    private fileTransferService: FileTransferService,
+  ) {}
 
   async getAllAvailableProducts(): Promise<Products[]> {
     return await this.dataSource
@@ -22,32 +31,45 @@ export class ProductsService {
 
   async createNewProduct(
     product: CreateProductDto,
+    file: FileTransferInterface,
   ): Promise<ProductCreatedResponse> {
-    const { name, img, price, dateOfBuy, amount } = product;
-    if (!name || !img || !price || !dateOfBuy || !amount) {
+    const { name, price, dateOfBuy, amount } = product;
+    const photo = file;
+    if (!name || !price || !dateOfBuy || !amount) {
       return { isSuccess: false, message: 'All values are necessary!' };
     } else if (name.length < 3) {
       return {
         isSuccess: false,
         message: `${name} length have to be greater than 2`,
       };
-    } else {
+    }
+    try {
       const newProduct = new Products();
       newProduct.name = name;
-      newProduct.price = price;
-      newProduct.amount = amount;
+      newProduct.price = Number(price);
+      newProduct.amount = Number(amount);
       newProduct.dateOfBuy = dateOfBuy;
-      newProduct.img = img;
+      if (photo) {
+        newProduct.img = photo.filename;
+      }
       await newProduct.save();
       return {
         isSuccess: true,
         message: `product ${name} successfully created!`,
       };
+    } catch (e1) {
+      try {
+        if (photo) {
+          fs.unlinkSync(photo.path);
+        }
+      } catch (e2) {
+        throw e2;
+      }
+      throw e1;
     }
   }
-
   async getProductByID(id: string): Promise<Products> | null {
-    const product = Products.findOne({ where: { id } });
+    const product = await Products.findOne({ where: { id } });
     if (product) {
       return product;
     } else return null;
@@ -55,16 +77,27 @@ export class ProductsService {
 
   async updateProduct(
     product: UpdateProductDto,
+    file: FileTransferInterface,
   ): Promise<ProductUpdatedResponse> {
-    const { id, name, price, amount, dateOfBuy, img } = product;
+    const { id, name, price, amount, dateOfBuy } = product;
+    const photo = file;
     const productToUpdate = await Products.findOne({ where: { id } });
+
     if (productToUpdate) {
       if (name.length > 2) {
         productToUpdate.name = name;
-        productToUpdate.price = price;
+        productToUpdate.price = Number(price);
         productToUpdate.dateOfBuy = dateOfBuy;
-        productToUpdate.amount = amount;
-        productToUpdate.img = img;
+        productToUpdate.amount = Number(amount);
+        if (photo) {
+          const pathTo = path.join(
+            storageDir(),
+            `/product-photos/${productToUpdate.img}`,
+          );
+          console.log(pathTo);
+          fs.unlinkSync(pathTo);
+          productToUpdate.img = photo.filename;
+        }
         await productToUpdate.save();
         return {
           isSuccess: true,
