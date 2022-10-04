ad8d09d3b4cf5cd622fe66f1e61919bc378e70d8
PatrykKeska <PatrykKeska4@gmail.com>
2022-10-04 18:26:45 +0200

    * changed port for listen main.js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const core_1 = require('@nestjs/core');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const app_module_1 = require('./app.module');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const cookieParser = require('cookie-parser');
 
 async function bootstrap() {
