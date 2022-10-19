40094d9ed62d77fee5af85cda23e9470b8579a65
PatrykKeska <PatrykKeska4@gmail.com>
2022-10-19 20:46:58 +0200

    * In service added all necessary methods to handle file transfer from the backend to the frontend. In controller added all endpoints to handle file transfer from BE to FE

--- src/file-transfer/file-transfer.controller.ts
+++ src/file-transfer/file-transfer.controller.ts
@@ -1,16 +1,35 @@
-import { Body, Controller, Inject, Post, Res, UseGuards } from '@nestjs/common';
+import { Controller, Get, Inject, Param, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { FileTransferService } from './file-transfer.service';
-@Controller('api/file-transfer')
+
+@Controller('file-transfer')
 export class FileTransferController {
   constructor(
     @Inject(FileTransferService)
     private fileTransferService: FileTransferService,
   ) {}
+
   @UseGuards(AuthGuard('jwt'))
-  @Post('/photo')
-  async getPhoto(@Body() body, @Res() res: any): Promise<any> {
-    const { id } = body;
-    return this.fileTransferService.getPhoto(id, res);
+  @Get('/product-photo/:id')
+  async getProductImg(@Param('id') id, @Res() res: any) {
+    return this.fileTransferService.getProductPhoto(id, res);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('/place-photo/:id')
+  async getPlaceImg(@Param('id') id, @Res() res: any) {
+    return this.fileTransferService.getPlacePhoto(id, res);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('/default-product')
+  async getDefaultProductImg(@Res() res: any) {
+    return this.fileTransferService.getDefaultProductPhoto(res);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('/default-place')
+  async getDefaultPlaceImg(@Res() res: any) {
+    return this.fileTransferService.getDefaultPlacePhoto(res);
   }
 }
--- src/file-transfer/file-transfer.service.ts
+++ src/file-transfer/file-transfer.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { Products } from '../entities/products.entity';
 import { storageDir } from '../utils/storage';
 import * as path from 'path';
+import { Places } from '../entities/places.entity';
 
 @Injectable()
 export class FileTransferService {
-  async getPhoto(id: string, res: any) {
+  async getProductPhoto(id: string, res: any) {
     try {
       const one = await Products.findOne({ where: { id } });
       if (!one) {
@@ -15,6 +16,42 @@ export class FileTransferService {
         throw new Error('No photo found');
       }
       res.sendFile(one.img, {
+        root: path.join(storageDir(), '/product-photos/'),
+      });
+    } catch (error) {
+      res.json({ error });
+    }
+  }
+  async getPlacePhoto(id: string, res: any) {
+    try {
+      const one = await Places.findOne({ where: { id: id } });
+      if (!one) {
+        throw new Error('No object found');
+      }
+      if (!one.img) {
+        throw new Error('No photo found');
+      }
+      res.sendFile(one.img, {
+        root: path.join(storageDir(), '/product-photos/'),
+      });
+    } catch (error) {
+      res.json({ error });
+    }
+  }
+
+  async getDefaultProductPhoto(res: any) {
+    try {
+      res.sendFile('default-product-image.jpeg', {
+        root: path.join(storageDir(), '/product-photos/'),
+      });
+    } catch (error) {
+      res.json({ error });
+    }
+  }
+
+  async getDefaultPlacePhoto(res: any) {
+    try {
+      res.sendFile('default-office-image.jpeg', {
         root: path.join(storageDir(), '/product-photos/'),
       });
     } catch (error) {
